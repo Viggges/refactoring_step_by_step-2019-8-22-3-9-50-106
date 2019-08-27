7f97ae3396528ca0af271ef71504e2a38ce20fd1
lw <623656827@qq.com>
2019-08-27 11:03:45 +0800

    11111111

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,9 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+	public int age;
 
-    public Driver(int age) {
-        this.age = age;
-    }
+	public Driver(int age) {
+		this.age = age;
+	}
 }
--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,10 @@
 package com.tws.refactoring;
 
 public class Police {
-    public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
-    }
+	public boolean checkDriver(Driver driver) {
+		if (driver.age >= 18)
+			return true;
+		else
+			return false;
+	}
 }
